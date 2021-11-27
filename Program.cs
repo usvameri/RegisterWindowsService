cf7de9301e049b98a733c62ee30a450d1a0e1073
Yusuf Ç <usvameri@outlook.com>
2021-11-28 02:28:28 +0300

    register a service or delete

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace ServiceRegisterer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                DeleteService("LicenceWorkorder");
+                RegisterService("LicenceWorkorder");
+                StartService("LicenceWorkorder");
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex);
+                Console.ReadKey();
+            }
+            
+
+        }
+    
+        static void RegisterService(string serviceName)
+        {
+
+            Process proc = new Process(); //call new Process
+            ProcessStartInfo info = new ProcessStartInfo(); //call new ProcessStartInfo
+            info.Arguments = string.Format(@"create {0} binpath=c:\checklicence\LicenceWorkorder.exe start=auto",serviceName); //set the arguments)
+            info.FileName = "sc.exe"; //set the file name (location)
+            info.UseShellExecute = true;
+            info.Verb = "runas";
+            proc.StartInfo = info; //put the StartInfo into the Procces method
+            proc.Start(); //Start the procces (sc.exe with the arguments)
+            proc.WaitForExit(); //waits till the procces is done
+        }
+        static void DeleteService(string serviceName)
+        {
+
+            Process proc = new Process(); 
+            ProcessStartInfo info = new ProcessStartInfo(); 
+            info.Arguments = string.Format(@"delete {0}", serviceName); )
+            info.FileName = "sc.exe"; 
+            info.UseShellExecute = true;
+            info.Verb = "runas";
+            proc.StartInfo = info; 
+            proc.Start(); 
+            proc.WaitForExit(); 
+        }
+        static void StartService(string serviceName)
+        {
+            Process proc = new Process();
+            ProcessStartInfo info = new ProcessStartInfo();
+            info.Arguments = string.Format(@"start {0}", serviceName);
+            info.FileName = "sc.exe";
+            info.UseShellExecute = true;
+            info.Verb = "runas";
+            proc.StartInfo = info;
+            proc.Start();
+            proc.WaitForExit(); 
+        }
+    }
+}
